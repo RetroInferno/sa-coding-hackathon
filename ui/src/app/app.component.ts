5f0992f99837b363d96401277916b3cf33905ab9
Zhaowei Ding <86904225+RetroInferno@users.noreply.github.com>
2023-10-09 11:23:03 -0400

    Minor code cleanup

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CheckboxRequiredValidator } from '@angular/forms';
 import { SwUpdate, VersionEvent } from '@angular/service-worker';
 
 @Component({
